fdcb25760bf32b7b1309b285fb23b9875d8aaaa5
abe <abe_Vos@msn.com>
2017-04-22 12:28:14 +0200

    Basic cat controls

---
+++ Assets/scripts/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+	[SerializeField]
+	float camera_distance = 5f;
+
+	private Camera camera;
+
+	private Quaternion rest_direction;
+	private Vector3 forward;
+
+	public Camera Camera { get { return camera; } }
+
+	public Vector3 Forward { get { return forward; } }
+	public Vector3 Right { get { return camera.transform.right; } }
+
+	protected void Awake()
+	{
+		camera = Camera.main;
+
+		rest_direction = camera.transform.rotation;
+		forward = Vector3.Cross (camera.transform.right, Vector3.up);
+	}
+
+	protected void Update()
+	{
+		Debug.DrawRay (camera.transform.position, camera_distance * (rest_direction * Vector3.forward));
+
+		camera.transform.position = Vector3.Lerp (camera.transform.position,
+			transform.position - camera_distance * (rest_direction * Vector3.forward),
+			5f * Time.deltaTime);
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	private float movement_speed = 1f;
+
+	private CharacterController controller;
+	private CameraManager camera;
+
+	private Vector3 direction;
+
+	protected void Awake()
+	{
+		controller = GetComponent<CharacterController> ();
+		camera = GetComponent<CameraManager> ();
+
+		direction = new Vector3 ();
+	}
+
+	protected void Update()
+	{
+		direction = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.W))
+		{
+			direction += camera.Forward;
+		}
+		else if (Input.GetKey (KeyCode.S))
+		{
+			direction -= camera.Forward;
+		}
+
+		if (Input.GetKey (KeyCode.D))
+		{
+			direction += camera.Right;
+		}
+		else if (Input.GetKey (KeyCode.A))
+		{
+			direction -= camera.Right;
+		}
+
+		direction = direction.normalized * movement_speed;
+
+		controller.SimpleMove (direction);
+	}
+}
