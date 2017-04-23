3404760318032dbf041cbb752deb231dce19b02d
Mathijs Lemmers <mathijs750@gmail.com>
2017-04-23 17:37:10 +0200

    Add notyfies to animation

---
+++ Assets/scripts/NotifySender.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NotifySender : MonoBehaviour
+{ 
+    [SerializeField]
+    private UnityEvent notifyEvent;
+
+    public void Notify()
+    {
+        notifyEvent.Invoke();
+    }
+}
