3c051ef9ab47fec5e3587c9c285ca7596166464a
Mathijs Lemmers <mathijs750@gmail.com>
2017-05-19 17:02:36 +0200

    Add (partial) audio randomizer

---
+++ Assets/scripts/RandomModulatedAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomModulatedAudio : MonoBehaviour
+{
+    [SerializeField]
+    private Dictionary<string,AudioClip[]> namedAudioClips;
+    [SerializeField]
+    private float pitchModulation;
+
+    private AudioSource audioSource;
+
+    public void Play(string parameterName = "")
+    {
+        audioSource.pitch = Random.Range(1 - pitchModulation, 1 + pitchModulation);
+        
+    }
+
+    // Use this for initialization
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        foreach (var clips in namedAudioClips.Values)
+        {
+            foreach (var audioClip in clips)
+            {
+                audioClip.LoadAudioData();
+            }
+        }
+    }
+}
